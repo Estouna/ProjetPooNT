ac1b1d8ad43516b10dce74fddbf8b724b12a829f
Estouna <cyril.depont@yahoo.fr>
2022-06-30 14:03:01 +0200

    30/062022 attributs formulaires

---
+++ Views/users/login.php
@@ -0,0 +1,9 @@
+<form action="#" method="post">
+    <label for="email">E-mail :</label>
+    <input type="email" name="email" id="email" class="form-control" required>
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" id="password" class="form-control" autofocus>
+    
+    <button class="btn btn-primary">Me connecter</button>
+</form>
