af9546dd3f5b3dfeb4a9b04caa021b9183c4c222
Estouna <cyril.depont@yahoo.fr>
2022-07-06 13:18:40 +0200

    06/07/2022

--- Views/users/login.php
+++ Views/users/login.php
@@ -1,10 +1,25 @@
 <h1 class="text-primary text-center mb-5">Connexion</h1>
 
 <!-- 
+    -------------------------------------------------------- MESSAGE D'ERREUR -------------------------------------------------------- 
+-->
+<?php if (!empty($_SESSION['erreur'])) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?php
+        echo $_SESSION['erreur'];
+        unset($_SESSION['erreur']);
+        ?>
+    </div>
+<?php endif; ?>
+
+<!-- 
     -------------------------------------------------------- FORMULAIRE DE CONNEXION-------------------------------------------------------- 
 -->
 <?= $loginForm ?>
 
+<!-- 
+    ------ BOUTON ------
+-->
 <div class="text-center">
     <a href="/users/register">Pas encore inscrit - S'inscrire</a>
 </div>
--- Views/users/register.php
+++ Views/users/register.php
@@ -1,6 +1,18 @@
 <h1 class="text-primary text-center mb-5">Inscription</h1>
 
 <!-- 
+    -------------------------------------------------------- MESSAGE D'ERREUR -------------------------------------------------------- 
+-->
+<?php if (!empty($_SESSION['erreur'])) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?php
+        echo $_SESSION['erreur'];
+        unset($_SESSION['erreur']);
+        ?>
+    </div>
+<?php endif; ?>
+
+<!-- 
     -------------------------------------------------------- FORMULAIRE D'INSCRIPTION -------------------------------------------------------- 
 -->
 <?= $registerForm ?>
