a9479dd91b6a21d79305cb7ac505a54d1fcb8192
Estouna <cyril.depont@yahoo.fr>
2022-08-16 17:57:31 +0200

    16/08/2022 sous-cat affichées sous les cat

--- Views/categories/index.php
+++ Views/categories/index.php
@@ -3,8 +3,25 @@
 -->
 <h1 class="text-primary text-center my-5">Catégories</h1>
 
-<?php foreach ($categories as $category) : ?>
-    <div class="text-center border border-primary my-4 p-2 rounded">
-        <h2><a href="/categories/sous_categories/<?= $category->id ?>"><?= $category->name ?></a></h2>
-    </div>
-<?php endforeach; ?>
+<div class="row justify-content-center">
+    <?php foreach ($categories as $category) : ?>
+
+        <div class="col-lg-3 col-md-4 col-sm-5 text-center m-2 py-4">
+            <h2><a href="/categories/sous_categories/<?= $category->id ?>"><?= $category->name ?></a></h2>
+            <?php foreach ($subCat as $sc) : ?>
+
+                <?php if (isset($category->id) && $sc->parent_id === $category->id) : ?>
+
+                    <?php if ($sc->rght - $sc->lft !== 1) { ?>
+                        <p><a href="/categories/sous_categories/<?= $sc->id ?>"><?= $sc->name ?></a></p>
+                    <?php } else { ?>
+                        <p><a class="text-dark" href="/categories/annonces/<?= $sc->id ?>"><?= $sc->name ?></a></p>
+                    <?php } ?>
+                    
+                <?php endif; ?>
+
+            <?php endforeach; ?>
+        </div>
+
+    <?php endforeach; ?>
+</div>
